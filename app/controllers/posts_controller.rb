73f92ac6ee36e1f62f3256738eda864c26ee29b3
pntifa <ptifa02@gmail.com>
2024-02-23 07:05:08 +0200

    service objects addition

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,14 @@
 class PostsController < ApplicationController
     def show
-        @post = Post.find(params[:id])
+        @post = Post.all
     end
 
     def get_posts
-        branch = params[:action]
-        search = params[:search]
-        category = params[:category]
-      
-        if category.blank? && search.blank?
-          posts = Post.by_branch(branch).all
-        elsif category.blank? && search.present?
-          posts = Post.by_branch(branch).search(search)
-        elsif category.present? && search.blank?
-          posts = Post.by_category(branch, category)
-        elsif category.present? && search.present?
-          posts = Post.by_category(branch, category).search(search)
-        else
-        end
+      PostsForBranchService.new({
+        search: params[:search],
+        category: params[:category],
+        branch: params[:action]
+      }).call
     end
       
     def hobby
