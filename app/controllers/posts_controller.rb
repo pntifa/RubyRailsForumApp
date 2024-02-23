fdeaa93e5bf585e4e5797e4cd38d6a7b90f9780e
pntifa <ptifa02@gmail.com>
2024-02-23 07:52:05 +0200

    post models validation + create post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+
     def show
-        @post = Post.all
+      @post = Post.find(params[:id])
     end
 
     def get_posts
@@ -19,7 +21,7 @@ class PostsController < ApplicationController
       else
       end
     end
-    
+
     def hobby
         posts_for_branch(params[:action])
     end
@@ -33,14 +35,33 @@ class PostsController < ApplicationController
     end
 
     private
+      def posts_for_branch(branch)
+          @categories = Category.where(branch: branch)
+          @posts = get_posts.paginate(page: params[:page])
 
-    def posts_for_branch(branch)
-        @categories = Category.where(branch: branch)
-        @posts = get_posts.paginate(page: params[:page])
+          respond_to do |format|
+              format.html
+              format.js { render partial: 'posts/posts_pagination_page' }
+          end
+      end
 
-        respond_to do |format|
-            format.html
-            format.js { render partial: 'posts/posts_pagination_page' }
-        end
+      def post_params
+        params.require(:post).permit(:content, :title, :category_id)
+                             .merge(user_id: current_user.id)
+      end
+
+    def new
+      @branch = params[:branch]
+      @categories = Category.where(branch: @branch)
+      @post = Post.new
+    end
+  
+    def create
+      @post = Post.new(post_params)
+      if @post.save 
+        redirect_to post_path(@post) 
+      else
+        redirect_to root_path
+      end
     end
 end
