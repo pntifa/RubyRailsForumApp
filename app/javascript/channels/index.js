aa809b712e43f3c3ed2bdaeb001b870ece11fe43
pntifa <ptifa02@gmail.com>
2024-02-27 23:55:21 +0200

    notification channel + contact request broadcast job

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,3 +1,4 @@
 // Import all the channels to be used by Action Cable
 import "channels/conversation_channel"
 import "channels/conversations_channel"
+import "channels/notification_channel"
---
+++ app/jobs/contact_request_broadcast_job.rb
@@ -0,0 +1,7 @@
+class ContactRequestBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
